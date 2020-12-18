1046d44fdfd869c9142ce4ed8800294ddef65fc1
Irazum <conscientiaaa@gmail.com>
2020-12-19 00:18:40 +0300

    v0.93 cl

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
 from .models import User, Categories, Listings, Bids, Comments
-# from django.forms import TextInput, Textarea
-# from django.db import models
 
 
 class ListingsAdmin(admin.ModelAdmin):
@@ -10,18 +8,6 @@ class ListingsAdmin(admin.ModelAdmin):
 
 class CommentsAdmin(admin.ModelAdmin):
     list_display = ['listing', 'user', 'comment_view']
-
-    # # need to change view of the data in “add” and “change” admin-pages
-    # formfield_overrides = {
-    #     models.TextField: {'widget': Textarea(attrs={'rows': 4, 'cols': 40})}
-    # }
-
-    # # if we use the funcs, we loss ability to sort column in admin panel
-    # def listing_id(self, obj):
-    #     return obj.listing.pk
-    #
-    # def listing_title(self, obj):
-    #     return obj.listing.title
 
 
 class BidsAdmin(admin.ModelAdmin):
